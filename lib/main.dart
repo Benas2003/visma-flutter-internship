4912d490ca0dc6e2562b6e99f9eff71f6584d4b1
benaskubilius <kubiliusb2003@gmail.com>
2022-05-09 19:23:55 +0300

    change(main): initial screen changed to ArticlesScreen()

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news_application/articles_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,38 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Articles'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Articles list',
-            )
-          ],
-        ),
-      ),
+      home: ArticlesScreen(),
     );
   }
 }
