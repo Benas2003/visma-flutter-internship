7c44bfca2f1b049d9d8a60cb76e4f80fa8294a05
benaskubilius <kubiliusb2003@gmail.com>
2022-05-09 19:18:04 +0300

    feat(article): added article model

---
+++ lib/models/article.dart
@@ -0,0 +1,18 @@
+class Article{
+  final String id;
+  final String heading;
+  final String imageUrl;
+  final DateTime publishDate;
+  final String author;
+  final String articleUrl;
+
+  Article({
+    required this.id,
+    required this.heading,
+    required this.imageUrl,
+    required this.publishDate,
+    required this.author,
+    required this.articleUrl,
+  });
+
+}
