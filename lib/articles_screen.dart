38b0f6454965761f89225bd106bcc35f53c1e0df
benaskubilius <kubiliusb2003@gmail.com>
2022-05-09 19:23:55 +0300

    change(main): initial screen changed to ArticlesScreen()

--- lib/articles_screen.dart
+++ lib/articles_screen.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 
 class ArticlesScreen extends StatelessWidget {
+  const ArticlesScreen({Key? key}) : super(key: key);
+
   @override
+
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.all(8),
-      children: <Widget>[
-        Container(
-          height: 50,
-          color: Colors.amber[600],
-          child: const Center(child: Text('Article A')),
-        ),
-      ],
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Articles'),
+      ),
+      body:
+      ListView(
+        padding: EdgeInsets.all(8),
+        children: <Widget>[
+          Container(
+            height: 50,
+            color: Colors.amber[600],
+            child: const Center(child: Text('Article A')),
+          ),
+        ],
+      ),
     );
   }
 }
