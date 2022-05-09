0dcd539909d50596dd988f1ba5b04ad88e6f04e0
benaskubilius <kubiliusb2003@gmail.com>
2022-05-09 20:33:52 +0300

    change(article): list of articles design changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_news_application/articles_screen.dart';
+import 'package:flutter_news_application/screens/articles_screen.dart';
 
 void main() {
   runApp(const MyApp());
