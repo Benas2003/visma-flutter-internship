1ff2c026ebea0f722b9479ee9f13a89b5e110b84
benaskubilius <kubiliusb2003@gmail.com>
2022-05-09 20:43:51 +0300

    update(Article): updated Article class

--- lib/models/article.dart
+++ lib/models/article.dart
@@ -2,6 +2,7 @@ class Article{
   final String id;
   final String heading;
   final String imageUrl;
+  final String shortDescription;
   final DateTime publishDate;
   final String author;
   final String articleUrl;
@@ -10,6 +11,7 @@ class Article{
     required this.id,
     required this.heading,
     required this.imageUrl,
+    required this.shortDescription,
     required this.publishDate,
     required this.author,
     required this.articleUrl,
