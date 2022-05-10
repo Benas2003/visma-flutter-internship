bfe01814599ce35ccd49d3e36309ebfaa615afeb
benaskubilius <kubiliusb2003@gmail.com>
2022-05-10 22:47:50 +0300

    feat(Article list): using news_api_flutter package created list of articles

--- lib/screens/articles_screen.dart
+++ lib/screens/articles_screen.dart
@@ -1,86 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news_application/screens/custom_article_screen.dart';
+
+import 'package:news_api_flutter_package/news_api_flutter_package.dart';
+import 'package:news_api_flutter_package/model/article.dart';
+import 'package:news_api_flutter_package/model/error.dart';
+
+
 
 class ArticlesScreen extends StatelessWidget {
-  const ArticlesScreen({Key? key}) : super(key: key);
+  final NewsAPI _newsAPI = NewsAPI("8a658ba592014d589ff59930a49294d6");
+
+  late final Future<List<Article>> articlesData = _newsAPI.getEverything(query: 'Tesla');
+
+  ArticlesScreen({Key? key}) : super(key: key);
 
   @override
-
   Widget build(BuildContext context) {
-
-    // return MaterialApp(
-    //   title: "Articles",
-    //   theme: ThemeData(
-    //     primaryColor: Colors.white,
-    //   ),
-    // )
-
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-            children: <Widget>[
-            const Text('Articles'),
-        ],
+        appBar: _buildAppBar(),
+        body: _buildListTabView(),
+    );
+  }
 
-        ),
-      ),
-      body:
-      ListView(
-        padding: EdgeInsets.all(10),
-        children: <Widget>[
-          Container(
-            height: 200,
-            //color: Colors.white,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10)
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 3), // changes position of shadow
-                ),
-              ],
-            ),
-            child: Row(
-              children:[
-                Image(
-                    image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                  ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: const Text(
-                          'Wall St slides as higher treasury yields hit growth stocks; Nasdaq down 3.4%',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: Text(
-                          'Rivian\'s stock price fell more than 17% Monday, a drop prompted by a CNBC report that Ford was selling 8 million shares of the EV automaker.',
-                          style: TextStyle(
-                            color: Colors.grey[500],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: const Text("Flutter News"),
+    );
+  }
+
+
+  Widget _buildListTabView() {
+    return FutureBuilder<List<Article>>(
+        future: _newsAPI.getEverything(query: 'Boring'),
+        builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
+          return snapshot.connectionState == ConnectionState.done
+              ? snapshot.hasData
+              ? _buildArticleListView(snapshot.data!)
+              : _buildError(snapshot.error as ApiError)
+              : _buildProgress();
+        });
+  }
+
+  Widget _buildArticleListView(List<Article> articles) {
+    return 
+      RefreshIndicator(
+        child: ListView.builder(
+          physics: const AlwaysScrollableScrollPhysics(),
+          itemCount: articles.length,
+          itemBuilder: (context, index) {
+            Article article = articles[index];
+          return Card(
+              child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CustomArticleScreen(customArticle: article)),
+                  );
+                },
+
+                child: ListTile(
+                  trailing: article.urlToImage == null
+                      ? null
+                      : Image.network(article.urlToImage!),
+                  title: Text(article.title!, maxLines: 2),
+                  subtitle: Text(article.description ?? "", maxLines: 3),
+                 ),
+               ),
+            );
+          },
           ),
-        ],
+          onRefresh: () {
+            FutureBuilder<List<Article>>(
+                future: _newsAPI.getEverything(query: 'Boring'),
+                builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
+                  return snapshot.connectionState == ConnectionState.done
+                      ? snapshot.hasData
+                      ? _buildArticleListView(snapshot.data!)
+                      : _buildError(snapshot.error as ApiError)
+                      : _buildProgress();
+                });
+              return _newsAPI.getEverything(query: 'Boring');
+          }
+      );
+  }
+
+  Widget _buildProgress() {
+    return const Center(child: CircularProgressIndicator(color: Colors.orange,));
+  }
+
+  Widget _buildError(ApiError error) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              error.code ?? "",
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 4),
+            Text(error.message!, textAlign: TextAlign.center),
+          ],
+        ),
       ),
     );
   }
